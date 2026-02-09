1667d308171e3a56c399ba02c5fb56a30eb610a9
brinersam <brinersam>
2026-02-09 23:39:33 +0300

    mvp more items

--- helldiversrogueliteapp/src/App.tsx
+++ helldiversrogueliteapp/src/App.tsx
@@ -1,17 +1,47 @@
 import "./App.css";
-import { weapons } from "./scripts/data/item_manifests";
+import { stratagems, weapons } from "./scripts/data/item_manifests";
 import ItemIcon from "./components/ItemData/ItemIcon";
 import type { weaponData } from "./scripts/defs/models/weaponData";
 import { useState } from "react";
+import type { stratagemData } from "./scripts/defs/models/stratagemData";
 
 function App() {
   const manifestPrimaries: weaponData[] = weapons.filter(
     (x) => x.weaponSlot == "Primary"
   );
+  const manifestSecondaries: weaponData[] = weapons.filter(
+    (x) => x.weaponSlot == "Secondary"
+  );
+  const manifestGemsRed: stratagemData[] = stratagems.filter(
+    (x) => x.stratagemType == "Red"
+  );
+  const manifestGemsBlue: stratagemData[] = stratagems.filter(
+    (x) => x.stratagemType == "Blue"
+  );
+  const manifestGemsGreen: stratagemData[] = stratagems.filter(
+    (x) => x.stratagemType == "Green"
+  );
 
-  const cssWeaponSize = { width: 200, height: 100 };
+  const cssWeaponSize = { width: 200, height: 110 };
+  const cssGenSize = { width: 100, height: 100 };
 
   const [primarySelection, setPrimarySelection] = useState<weaponData[]>([]);
+  const [secondarySelection, setSecondarySelection] = useState<weaponData[]>(
+    []
+  );
+  const [gem1, setGem1] = useState<stratagemData[]>([]);
+  const [gem2, setGem2] = useState<stratagemData[]>([]);
+  const [gem3, setGem3] = useState<stratagemData[]>([]);
+  const [gem4, setGem4] = useState<stratagemData[]>([]);
+
+  const resetAll = () => {
+    setPrimarySelection([]);
+    setSecondarySelection([]);
+    setGem1([]);
+    setGem2([]);
+    setGem3([]);
+    setGem4([]);
+  };
 
   const generateUniqueIdx = (n: number, source: unknown[]): number[] => {
     const totalItems = source.length;
@@ -36,31 +66,164 @@ function App() {
     return resultIdx;
   };
 
-  const rollPrimaries = () => {
-    const newPrimaries: weaponData[] = [];
-    generateUniqueIdx(3, manifestPrimaries).forEach(
-      (v, i) => (newPrimaries[i] = manifestPrimaries[v])
+  const rollItems = <T,>(n: number, source: T[]): T[] => {
+    const chosenItems: T[] = [];
+    generateUniqueIdx(n, source).forEach(
+      (v, i) => (chosenItems[i] = source[v])
     );
-
-    setPrimarySelection(newPrimaries);
+    return chosenItems;
   };
 
   return (
     <>
-      <button
-        onClick={rollPrimaries}
-        className="rounded-full bg-sky-500 px-5 py-2 text-sm leading-5 font-semibold text-white hover:bg-sky-700"
-      >
-        Get random primaries
-      </button>
-      <div style={cssWeaponSize} className={`overflow-hidden`}>
-        <ItemIcon data={primarySelection[0] ?? null}></ItemIcon>
+      <div>
+        <button
+          onClick={() => resetAll()}
+          className="rounded-full bg-red-400 px-40 py-2 text-sm leading-5 font-semibold text-white hover:bg-red-700"
+        >
+          {" "}
+          RESET{" "}
+        </button>
       </div>
-      <div style={cssWeaponSize} className={`overflow-hidden`}>
-        <ItemIcon data={primarySelection[1] ?? null}></ItemIcon>
+      {/* weapons bar*/}
+      <div style={{ display: "flex" }}>
+        <div>
+          <button
+            onClick={() => setPrimarySelection(rollItems(2, manifestPrimaries))}
+            className="rounded-full bg-sky-500 px-5 py-2 text-sm leading-5 font-semibold text-white hover:bg-sky-700"
+          >
+            Get random primaries
+          </button>
+          <div style={cssWeaponSize} className={`overflow-hidden`}>
+            <ItemIcon data={primarySelection[0]}></ItemIcon>
+          </div>
+          <div style={cssWeaponSize} className={`overflow-hidden`}>
+            <ItemIcon data={primarySelection[1]}></ItemIcon>
+          </div>
+        </div>
+        <div>
+          <button
+            onClick={() =>
+              setSecondarySelection(rollItems(2, manifestSecondaries))
+            }
+            className="rounded-full bg-sky-500 px-5 py-2 text-sm leading-5 font-semibold text-white hover:bg-sky-700"
+          >
+            Get random secondaries
+          </button>
+          <div style={cssWeaponSize} className={`overflow-hidden`}>
+            <ItemIcon data={secondarySelection[0]}></ItemIcon>
+          </div>
+          <div style={cssWeaponSize} className={`overflow-hidden`}>
+            <ItemIcon data={secondarySelection[1]}></ItemIcon>
+          </div>
+        </div>
       </div>
-      <div style={cssWeaponSize} className={`overflow-hidden`}>
-        <ItemIcon data={primarySelection[2] ?? null}></ItemIcon>
+      {/* gems bar*/}
+      <div style={{ display: "flex" }}>
+        <div>
+          <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+            <button
+              onClick={() => setGem1(rollItems(3, manifestGemsRed))}
+              className="rounded-full bg-red-400 px-3 py-5 text-sm leading-5 font-semibold text-white hover:bg-red-700"
+            />
+            <button
+              onClick={() => setGem1(rollItems(3, manifestGemsBlue))}
+              className="rounded-full bg-sky-500 px-3 py-5 text-sm leading-5 font-semibold text-white hover:bg-sky-700"
+            />
+            <button
+              onClick={() => setGem1(rollItems(3, manifestGemsGreen))}
+              className="rounded-full bg-green-500 px-3 py-5 text-sm leading-5 font-semibold text-white hover:bg-green-700"
+            />
+          </div>
+
+          <div style={cssGenSize} className={`overflow-hidden`}>
+            <ItemIcon data={gem1[0]}></ItemIcon>
+          </div>
+          <div style={cssGenSize} className={`overflow-hidden`}>
+            <ItemIcon data={gem1[1]}></ItemIcon>
+          </div>
+          <div style={cssGenSize} className={`overflow-hidden`}>
+            <ItemIcon data={gem1[2]}></ItemIcon>
+          </div>
+        </div>
+        <div>
+          <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+            <button
+              onClick={() => setGem2(rollItems(3, manifestGemsRed))}
+              className="rounded-full bg-red-400 px-3 py-5 text-sm leading-5 font-semibold text-white hover:bg-red-700"
+            />
+            <button
+              onClick={() => setGem2(rollItems(3, manifestGemsBlue))}
+              className="rounded-full bg-sky-500 px-3 py-5 text-sm leading-5 font-semibold text-white hover:bg-sky-700"
+            />
+            <button
+              onClick={() => setGem2(rollItems(3, manifestGemsGreen))}
+              className="rounded-full bg-green-500 px-3 py-5 text-sm leading-5 font-semibold text-white hover:bg-green-700"
+            />
+          </div>
+
+          <div style={cssGenSize} className={`overflow-hidden`}>
+            <ItemIcon data={gem2[0]}></ItemIcon>
+          </div>
+          <div style={cssGenSize} className={`overflow-hidden`}>
+            <ItemIcon data={gem2[1]}></ItemIcon>
+          </div>
+          <div style={cssGenSize} className={`overflow-hidden`}>
+            <ItemIcon data={gem2[2]}></ItemIcon>
+          </div>
+        </div>
+        <div>
+          <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+            <button
+              onClick={() => setGem3(rollItems(3, manifestGemsRed))}
+              className="rounded-full bg-red-400 px-3 py-5 text-sm leading-5 font-semibold text-white hover:bg-red-700"
+            />
+            <button
+              onClick={() => setGem3(rollItems(3, manifestGemsBlue))}
+              className="rounded-full bg-sky-500 px-3 py-5 text-sm leading-5 font-semibold text-white hover:bg-sky-700"
+            />
+            <button
+              onClick={() => setGem3(rollItems(3, manifestGemsGreen))}
+              className="rounded-full bg-green-500 px-3 py-5 text-sm leading-5 font-semibold text-white hover:bg-green-700"
+            />
+          </div>
+
+          <div style={cssGenSize} className={`overflow-hidden`}>
+            <ItemIcon data={gem3[0]}></ItemIcon>
+          </div>
+          <div style={cssGenSize} className={`overflow-hidden`}>
+            <ItemIcon data={gem3[1]}></ItemIcon>
+          </div>
+          <div style={cssGenSize} className={`overflow-hidden`}>
+            <ItemIcon data={gem3[2]}></ItemIcon>
+          </div>
+        </div>
+        <div>
+          <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+            <button
+              onClick={() => setGem4(rollItems(3, manifestGemsRed))}
+              className="rounded-full bg-red-400 px-3 py-5 text-sm leading-5 font-semibold text-white hover:bg-red-700"
+            />
+            <button
+              onClick={() => setGem4(rollItems(3, manifestGemsBlue))}
+              className="rounded-full bg-sky-500 px-3 py-5 text-sm leading-5 font-semibold text-white hover:bg-sky-700"
+            />
+            <button
+              onClick={() => setGem4(rollItems(3, manifestGemsGreen))}
+              className="rounded-full bg-green-500 px-3 py-5 text-sm leading-5 font-semibold text-white hover:bg-green-700"
+            />
+          </div>
+
+          <div style={cssGenSize} className={`overflow-hidden`}>
+            <ItemIcon data={gem4[0]}></ItemIcon>
+          </div>
+          <div style={cssGenSize} className={`overflow-hidden`}>
+            <ItemIcon data={gem4[1]}></ItemIcon>
+          </div>
+          <div style={cssGenSize} className={`overflow-hidden`}>
+            <ItemIcon data={gem4[2]}></ItemIcon>
+          </div>
+        </div>
       </div>
     </>
   );
--- helldiversrogueliteapp/src/components/ItemData/ItemIcon.tsx
+++ helldiversrogueliteapp/src/components/ItemData/ItemIcon.tsx
@@ -1,6 +1,10 @@
 import type { itemData } from "../../scripts/defs/models/itemData";
 
 export default function ItemIcon({ data }: { data: itemData | null }) {
+  // stackoverflow.com/questions/19126185/setting-an-image-src-to-empty
+  const nullImage: string =
+    "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+
   return (
     <>
       <img
@@ -12,7 +16,7 @@ export default function ItemIcon({ data }: { data: itemData | null }) {
           height: "100%",
           objectFit: "fill",
         }}
-        src={data?.icon_url}
+        src={data?.icon_url ?? nullImage}
       ></img>
     </>
   );
