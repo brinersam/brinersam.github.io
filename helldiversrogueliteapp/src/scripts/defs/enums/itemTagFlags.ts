38ba925ff41df18e9cd0ffc569a763e18ea952d1
brinersam <brinersam>
2026-02-17 14:31:02 +0300

    Add damage types, item tags

--- helldiversrogueliteapp/src/scripts/defs/enums/itemTagFlags.ts
+++ helldiversrogueliteapp/src/scripts/defs/enums/itemTagFlags.ts
@@ -1,10 +1,11 @@
 export const itemTagFlags = {
     None : 0,
-    backpack_slot : 1 << 0,
-    support_weapon : 1 << 1,
+    Backpack_slot : 1 << 0,
+    Support_weapon : 1 << 1,
     SEAF : 1 << 2,
-    orbital : 1 << 3,
-    melee : 1 << 4
+    Orbital : 1 << 3,
+    Melee : 1 << 4,
+    OneHanded : 1 << 5
 } as const;
 
 export type itemTagFlags = typeof itemTagFlags[keyof typeof itemTagFlags];
--- helldiversrogueliteapp/src/scripts/defs/models/typeOfDamage.ts
+++ helldiversrogueliteapp/src/scripts/defs/models/typeOfDamage.ts
@@ -1,13 +1,12 @@
-export interface hasTypeOfDamage {
-    dmgType : dmgTypeFlags
-}
-
-export const dmgTypeFlags = //todo
+export const dmgTypeFlags =
 {
     None : 0,
     Boolet : 1 << 0,
     Energy : 1 << 1,
     SmallExplosive : 1 << 2,
     Explosive : 1 << 3,
+    Electricity : 1 << 4,
+    Incendiary : 1 << 5,
+    Stun : 1 << 6,
 } as const;
 export type dmgTypeFlags = typeof dmgTypeFlags[keyof typeof dmgTypeFlags];
