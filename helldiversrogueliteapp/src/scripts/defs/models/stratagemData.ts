8c5c5801c3bf10b886382edd91a769de34c39403
brinersam <brinersam>
2026-02-17 14:30:32 +0300

    Slight data reorganization

--- helldiversrogueliteapp/src/scripts/defs/models/stratagemData.ts
+++ helldiversrogueliteapp/src/scripts/defs/models/stratagemData.ts
@@ -1,11 +1,11 @@
 import type { itemData } from "./itemData";
-import type { hasTypeOfDamage } from "./typeOfDamage";
+import type { weaponData } from "./weaponData";
 
 export interface stratagemData extends itemData {
     stratagemType : stratagemTypeEnum
 }
 
-export interface stratagemWeaponData extends stratagemData, hasTypeOfDamage {}
+export interface stratagemWeaponData extends stratagemData, weaponData {}
 
 export const stratagemTypeEnum = ['Red','Blue','Green'] as const;
 export type stratagemTypeEnum = (typeof stratagemTypeEnum)[number];
--- helldiversrogueliteapp/src/scripts/defs/models/weaponData.ts
+++ helldiversrogueliteapp/src/scripts/defs/models/weaponData.ts
@@ -1,8 +1,9 @@
 import type { itemData } from "./itemData"
-import type { hasTypeOfDamage } from "./typeOfDamage";
+import type { dmgTypeFlags } from "./typeOfDamage";
 
-export interface weaponData extends itemData, hasTypeOfDamage {
+export interface weaponData extends itemData {
     weaponSlot : weaponSlotEnum,
+    dmgType : dmgTypeFlags
 }
 
 export const weaponSlotEnum = ['Primary', 'Secondary', 'Grenade', 'Support'] as const;
